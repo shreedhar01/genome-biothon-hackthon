d8101ec34998299a64d16c5f5f74b172a94eabe3
shreedhar01 <shreedharkalamagar@gmail.com>
2026-04-25 15:32:30 +0545

    feat: multer config file

---
+++ backend/src/config/multer.ts
@@ -0,0 +1,22 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs"
+
+const uploadPath = path.join(__dirname,"..", "uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  }
+});
+
+export const upload = multer({ storage: storage });
+
