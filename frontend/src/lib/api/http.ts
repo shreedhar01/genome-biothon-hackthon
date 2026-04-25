095acaa928b015f916166a88ae8530304a7faba1
shreedhar01 <shreedharkalamagar@gmail.com>
2026-04-25 18:54:59 +0545

    fix: port number

--- frontend/src/lib/api/http.ts
+++ frontend/src/lib/api/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1"
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8001/api/v1"
 
 
 export const http = axios.create({
